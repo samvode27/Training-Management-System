4287a0752fe3c76a50189f7dd5cc39e2af05eefc
Samuel Setarige <setarigesamuel@gmail.com>
2026-06-20 12:58:37 +0300

    Session1 - Fundamentals and null safety

--- obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
+++ obj/Debug/net10.0/TmsCore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54beee78d22f68a2cd17467d025c7faf270586f3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a604448eb4ad33b21fa9629f8fc0b23c32adc0f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsCore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
