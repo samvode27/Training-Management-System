cf973590b34bbabdd03dda9c8e7664e333d57189
Samuel Setarige <setarigesamuel@gmail.com>
2026-06-21 13:25:19 +0300

    feat: add student name

---
+++ Module3/session1.cs
@@ -0,0 +1,17 @@
+// Create Module 3 Branch
+git switch -c module-3-session-1
+
+//Create Professional README.md
+
+
+//git restore
+
+accidentally delete:
+
+public class Student
+{
+}
+
+git restore Student.cs 
+
+File comes back.
