2ffe275edfbc6593910e28dc4c2fbce03918317a
Samuel Setarige <setarigesamuel@gmail.com>
2026-06-25 17:38:26 +0300

    up and down, ExecuteUpdateAsync

--- Module5/session3.cs
+++ Module5/session3.cs
@@ -1,5 +1,24 @@
 // Performance
 
+// Up() and Down()
+
+// Up()  (Moves database forward)
+// protected override void Up(MigrationBuilder migrationBuilder)
+// {
+//     migrationBuilder.AddColumn<decimal>(
+//         name: "GPA",
+//         table: "Students");
+// }
+
+// // Down() (Moves database backward)
+// protected override void Down(MigrationBuilder migrationBuilder)
+// {
+//     migrationBuilder.DropColumn(
+//         name: "GPA",
+//         table: "Students");
+// }
+
+
 
 // N+1 Problem
 
@@ -12,77 +31,83 @@
 
 
 // Bad Version (Many database calls)
-var students = await context.Students.ToListAsync();
+// var students = await context.Students.ToListAsync();
 
-foreach(var student in students)
-{
-    Console.WriteLine(
-        context.Enrollments
-        .Count(e => e.StudentId == student.Id)
-    );
-}
+// foreach(var student in students)
+// {
+//     Console.WriteLine(
+//         context.Enrollments
+//         .Count(e => e.StudentId == student.Id)
+//     );
+// }
 
-// Better Version (Single database call)
-var students = await context.Students
-    .Include(s => s.Enrollments)
-    .ToListAsync();
+// // Better Version (Single database call)
+// var students = await context.Students
+//     .Include(s => s.Enrollments)
+//     .ToListAsync();
 
-foreach(var student in students)
-{
-    Console.WriteLine(student.Enrollments.Count);
-}
+// foreach(var student in students)
+// {
+//     Console.WriteLine(student.Enrollments.Count);
+// }
+
+
 
 
 // AsNoTracking()
 
-// Hard Delete
-context.Students.Remove(student);
-await context.SaveChangesAsync();
 
-// Soft Delete
-public bool IsDeleted { get; set; }   // add property
 
-student.IsDeleted = true;
-await context.SaveChangesAsync();
+// // Hard Delete
+// context.Students.Remove(student);
+// await context.SaveChangesAsync();
 
-// Automatically Hide Deleted Records
-protected override void OnModelCreating(ModelBuilder modelBuilder)
-{
-    modelBuilder.Entity<Student>()
-        .HasQueryFilter(s => !s.IsDeleted);
-}
+// // Soft Delete
+// public bool IsDeleted { get; set; }   // add property
 
-var students = await context.Students.ToListAsync();  // Only active students
+// student.IsDeleted = true;
+// await context.SaveChangesAsync();
+
+// // Automatically Hide Deleted Records
+// protected override void OnModelCreating(ModelBuilder modelBuilder)
+// {
+//     modelBuilder.Entity<Student>()
+//         .HasQueryFilter(s => !s.IsDeleted);
+// }
+
+// var students = await context.Students.ToListAsync();  // Only active students
+
+
 
 
 // ExecuteUpdateAsync()
 
-// Traditional Way
-var enrollments = await context.Enrollments.ToListAsync();
-foreach(var e in enrollments)
-{
-    e.IsArchived = true;
-}
-await context.SaveChangesAsync();
+// // Traditional Way
+// var enrollments = await context.Enrollments.ToListAsync();
+// foreach(var e in enrollments)
+// {
+//     e.IsArchived = true;
+// }
+// await context.SaveChangesAsync();
 
-// Better
-await context.Enrollments
-    .Where(e => e.EnrolledAt < cutoffDate)
-    .ExecuteUpdateAsync(
-        s => s.SetProperty(
-            e => e.IsArchived,
-            true));
+// // Better
+// await context.Enrollments
+//     .Where(e => e.EnrolledAt < cutoffDate)
+//     .ExecuteUpdateAsync(
+//         s => s.SetProperty(
+//             e => e.IsArchived,
+//             true));
 
-// example
-public class Enrrollment
-{
-    public int Id { get; set; }
+// // example
+// public class Enrrollment
+// {
+//     public int Id { get; set; }
 
-    public bool IsArchived { get; set; }
-}
+//     public bool IsArchived { get; set; }
+// }
 
-await context.Enrrollment
-    .ExecuteUpdateAsync(
-        s => s.SetProperty(
-            e => e.IsArchived,
-            true));                        // Every enrollment becomes archived.
+// await context.Enrrollment
+//     .ExecuteUpdateAsync(
+//         s => s.SetProperty(
+//             e => e.IsArchived,
+//             true));                        // Every enrollment becomes archived.
