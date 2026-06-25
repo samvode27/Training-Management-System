3fcbb5267e120570ee5ec70f92cf56bf8b265b65
Samuel Setarige <setarigesamuel@gmail.com>
2026-06-25 15:37:59 +0300

    Captive Dependency, Options Patter, Structured Logging

--- Module4/session2.cs
+++ Module4/session2.cs
@@ -1,12 +1,79 @@
-public class MyService
-{
-    public Guid Id { get; } = Guid.NewGuid();
-}
+// Captive Dependency
 
-builder.Services.AddTransient<MyService>();
+// public interface IEnrollmentService
+// {
+//     void EnrollStudent();
+// }
 
-var s1 = serviceProvider.GetService<MyService>();
-var s2 = serviceProvider.GetService<MyService>();
+// // Implementation
+// public class EnrollmentService : IEnrollmentService
+// {
+//     public void EnrollStudent()
+//     {
+//         Console.WriteLine("Student enrolled");
+//     }
+// }
 
-Console.WriteLine(s1.Id);
-Console.WriteLine(s2.Id);
+// // AddScoped(one instance per request) Request 1 -> Create EnrollmentService -> Use it -> Destroy it
+// builder.Services.AddScoped<IEnrollmentService, EnrollmentService>();
+
+
+// Singleton Service
+// public class EnrollmentWorker
+// {
+// }
+
+// // one instanse for all applications
+// builder.Services.AddSingleton<EnrollmentWorker>();
+
+
+
+
+// Exercise 3: Options Pattern
+// public class PaymentService
+// {
+//     public void Pay()
+//     {
+//         string gatewayUrl = "https://payment.com";
+//         decimal maxDeposit = 10000;
+//     }
+// }
+// if Gateway URL changes (Open code -> Change code -> Rebuild application -> Deploy again)
+
+// so add Configuration File
+// {
+//   "Payments": {
+//     "GatewayUrl": "https://payment.com",
+//     "MaxDepositBirr": 10000
+//   }
+// }
+
+// // then Take Payments section and fill PaymentOptions object
+// builder.Services
+//     .AddOptions<PaymentOptions>()
+//     .BindConfiguration("Payments");
+
+
+// ValidateOnStart()
+// builder.Services
+//     .AddOptions<PaymentOptions>()
+//     .BindConfiguration("Payments")
+//     .ValidateDataAnnotations()
+//     .ValidateOnStart();
+
+
+
+// Exercise 4: Structured Logging
+//  _logger.LogInformation(
+//   "Enrolling student {StudentId} in course {CourseCode}",
+//   studentId,
+//   courseCode);
+
+// // Duplicate Enrollment
+// _logger.LogWarning(
+//   "Duplicate enrollment attempt
+//    {StudentId}
+//    already in
+//    {CourseCode}",
+//    studentId,
+//    courseCode);
